34f9a08b8cb0fe72b37227a3d1ed5375c884308f
DEEPAK7525VERMA <145926307+DEEPAK7525VERMA@users.noreply.github.com>
2025-12-23 17:33:21 +0530

    first

---
+++ src/main/java/com/deepak/youtube/Springboot/DSAHello.java
@@ -0,0 +1,6 @@
+package com.deepak.youtube.Springboot;
+public class DSAHello {
+    public static void main(String[] args) {
+        System.out.println("Hello DSA Java");
+    }
+}
---
+++ src/main/java/com/deepak/youtube/Springboot/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.deepak.youtube.Springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+}
