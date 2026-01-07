85e62815c19b1fe26a73909aba998ff492290d0e
DEEPAK7525VERMA <145926307+DEEPAK7525VERMA@users.noreply.github.com>
2026-01-07 23:48:32 +0530

    feat: add User entity, repository and initial DB insert

--- src/main/java/com/deepak/youtube/Springboot/SpringbootApplication.java
+++ src/main/java/com/deepak/youtube/Springboot/SpringbootApplication.java
@@ -1,5 +1,7 @@
 package com.deepak.youtube.Springboot;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,17 @@ public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+
 		Ninja ninja = new Naruto();
 		ninja.fight();
 	}
 
+	@Bean
+	public CommandLineRunner demo(UserRepository repo) {
+		return args -> {
+			User u = new User("Naruto", "naruto@leaf.com");
+			repo.save(u);
+			System.out.println("User saved to DB!");
+		};
+	}
 }
---
+++ src/main/java/com/deepak/youtube/Springboot/UserRepository.java
@@ -0,0 +1,7 @@
+package com.deepak.youtube.Springboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
